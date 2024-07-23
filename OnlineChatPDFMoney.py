9823905f1b7da9d257986756828fecae3fdafc87
Nayeongseo0217 <nays43883@naver.com>
2024-07-23 16:49:33 +0900

    Update OnlineChatPDFMoney.py

--- OnlineChatPDFMoney.py
+++ OnlineChatPDFMoney.py
@@ -51,14 +51,14 @@ def pdf_to_document(uploaded_file):
     return pages
 
 #Stream 받아 줄 Hander 만들기
-'''class Streamhandler(BaseCallbackHandler):
-    def __init__(self, container, initial_text = ""):
-        self.container = container
-        self.text = initial_text
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.markdown(self.text)'''
-class StreamHandler:
+#class Streamhandler(BaseCallbackHandler):
+#    def __init__(self, container, initial_text = ""):
+#        self.container = container
+#        self.text = initial_text
+#    def on_llm_new_token(self, token: str, **kwargs) -> None:
+#        self.text += token
+#        self.container.markdown(self.text)
+class StreamHandler(BaseCallbackHandler):
     def __init__(self, chat_box):
         self.chat_box = chat_box
     
